b09b66d76b8772450880b204131f9f7efa5a19c3
JuniorYoung <yjunior@foxmail.com>
2018-09-09 20:21:04 +0800

    add async action example

---
+++ src/c13/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Root from './containers/Root';
+
+export default () => (
+    <div>
+        <div>
+            <a href='http://demo.rekit.org/element/src%2Ffeatures%2Fhome%2Fredux%2FsaveFile.js/code'>
+                http://demo.rekit.org/element/src%2Ffeatures%2Fhome%2Fredux%2FsaveFile.js/code
+            </a>
+        </div>
+        <Root />
+    </div>
+)
